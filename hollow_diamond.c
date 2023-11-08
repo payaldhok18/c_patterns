d11a811593a7b53efa472dcdc3374eccddfa519f
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2023-11-08 12:05:35 +0530

    Create hollow_diamond.c

---
+++ hollow_diamond.c
@@ -0,0 +1,42 @@
+/*
+	*
+       * *
+      *   *
+     *     *
+      *   *
+       * *
+        *
+        */
+#include <stdio.h>
+int main()
+{
+	int N;
+	printf("Enter your number : ");
+	scanf("%d",&N);
+	for(int i=1;i<=1;i++)
+	{
+		for(int k=1;k<=N-i;k++)
+		{
+			printf(" ");
+		}
+		if(i==1)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(int j=2;j<=N;j++)
+	{
+		for(int k=2;k<=N-j;k++)
+		{
+			printf(" ");
+		}
+		printf("* ");
+		for(int i=1;i<=N-i;i++)
+		{
+			printf("  ");
+		}
+		printf("*\n");
+		
+	}
+}
