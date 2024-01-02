bb33a8269c38df6a5363237b0b1fc31dafc00ea7
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2024-01-02 11:43:16 +0530

    Create fill_diamond.c

---
+++ fill_diamond.c
@@ -0,0 +1,38 @@
+/*
+   *
+  * *
+ * * *
+  * *
+   * 
+#include <stdio.h>
+int main()
+{
+	int N;
+	printf("Enter your number : ");
+	scanf("%d",&N);
+	for(int k=1;k<=N;k++)
+	{
+		for(int i=1;i<=N-k;i++)
+		{
+			printf(" ");
+		}
+		for(int j=1;j<=k;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+        }
+        for(int k=1;k<=N;k++)
+        {	
+        	for(int i=1;i<=k;i++)
+        	{
+        		printf(" ");
+        	}
+        	for(int j=1;j<=N-k;j++)
+        	{
+        		printf("* ");
+        	}
+        	printf("\n");
+        }
+}
+*/
