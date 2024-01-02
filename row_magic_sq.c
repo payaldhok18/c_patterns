46d1a83803fea516040ff93306f59c8c0caa6152
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2024-01-02 11:19:55 +0530

    Create row_magic_sq.c

---
+++ row_magic_sq.c
@@ -0,0 +1,23 @@
+#include <stdio.h>//row magic square
+int main()
+{
+	int n,i=0,k=1;
+	printf("Enter no.: ");
+	scanf("%d",&n);
+	int arr[n][n];
+	for(i;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			arr[i][j]=k;
+			k++;
+		}	
+	}
+	for(int i=0;i<n;i++)
+	{	for(int j=0;j<n;j++)
+	 	{
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+}
